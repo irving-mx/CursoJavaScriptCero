64ea4a0fa6f83667423be54ada40d25d1d74afa7
Irving mtz <al2173074687@azc.uam.mx>
2024-07-19 03:28:56 -0600

    Caracteres de escape, varias maneras de dar salto de linea o respetar cierto caracter

--- PLATZI/clase-string.js
+++ PLATZI/clase-string.js
@@ -42,3 +42,46 @@ const var2 = 'poderosisima UAM👨‍🎓'
 const var3 = 'y soy de Iguala,Gro☀️'
 const result = 'Un poco sobre mi: '.concat(var1,', ',var2,', ',var3)
 console.log(result)
+
+
+// Caracteres de escape
+// const whatDoIDo = 'I'm Software Engenieer'
+
+// 1. Escaper alternativo
+const escapeAlternativo = "I'm Software Engineer"
+
+// 2. Barra invertida
+const escapeBarraInvertida = 'I\'m Software Engineer'
+
+// 3. Template literals 
+const escapeComillaInvertida = `I'm Software Engineer`
+
+// Escritura de Strings Largos
+
+/*
+    El exito es para los que,
+    lo desean, la disiplina es,
+    indespensable para alcanzar,
+    los objetivos y la motivacion,
+    de dia a dia, para construir tu futuro
+*/ 
+const poema = 'El exito es para los que,\n' +
+                'lo desean, la disiplina es,\n'+
+                'indespensable para alcanzar,\n'+
+                'los objetivos y la motivacion,\n'+
+                'de dia a dia, para construir tu futuro'
+// console.log(poema)
+
+const poema2 =  'El exito es para los que,\n\
+lo desean, la disiplina es,\n\
+los objetivos y la motivacion,\n\
+de dia a dia, para construir tu futuro'
+// console.log(poema2)
+
+const poema3 =  `El exito es para los que,
+lo desean, la disiplina es,
+los objetivos y la motivacion,
+de dia a dia, para construir tu futuro`
+console.log(poema3)
+
+
