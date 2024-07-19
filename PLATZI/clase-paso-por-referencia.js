ad19590175d1c46b3d8a5cbe4c1e8381dbfd4bed
Irving mtz <al2173074687@azc.uam.mx>
2024-07-19 00:51:50 -0600

    paso por referencia

---
+++ PLATZI/clase-paso-por-referencia.js
@@ -0,0 +1,19 @@
+// Tipo de dato complejo - Paso por referencia
+
+let frutas ={
+    naranja:'🍊'
+}
+
+let vegetales = frutas
+
+vegetales.naranja = '🥦'
+console.log(frutas)
+
+
+
+let ropa = {
+    blusa:'👘'
+}
+
+ropa.pantalon = '👖'
+console.log(ropa)
