de456394b263a85084f06bcb994f7fee2ebad570
Irving mtz <al2173074687@azc.uam.mx>
2024-07-24 04:16:12 -0600

    ejemplo de condicional IF

---
+++ PLATZI/ciclos/ejecucionCondicional/if/index.js
@@ -0,0 +1,10 @@
+let nombre = "Nico";
+
+if(nombre === "Diego"){
+    console.log("Hola Diego Melo")
+}else if(nombre === "Nico"){
+    console.log("Hola Nico")
+}else {
+    console.log("Nombre no encontrado")
+
+}
