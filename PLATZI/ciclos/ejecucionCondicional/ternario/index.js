0cd54a3462121ce8c8a0c5ac6a97d34164c98cc9
Irving mtz <al2173074687@azc.uam.mx>
2024-07-25 00:53:03 -0600

    uso del operador ternario condicion ? true: false

---
+++ PLATZI/ciclos/ejecucionCondicional/ternario/index.js
@@ -0,0 +1,18 @@
+let mensaje 
+
+const edad = 20;
+
+if(edad >= 18){
+    mensaje = "Es mayor de edad, puede pasar"
+}else{
+    mensaje ="No es mayor de edad, no puede pasar"
+}
+
+console.log(mensaje)
+
+console.log("_______________________")
+// Ejecución condicional ternario - Operador Ternario
+// condicion ? true : false
+const años = 10
+let mensajeT  = años >= 20 ? "Eres mayor o igual a 20 años" : "Eres menor de 20 ";
+console.log(mensajeT)
