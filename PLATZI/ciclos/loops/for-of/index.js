4e2dc907088e83aaec7512c2fc817145bbfcc1d5
Irving mtz <al2173074687@azc.uam.mx>
2024-07-25 03:30:16 -0600

    Ejemplo de ciclo for of

---
+++ PLATZI/ciclos/loops/for-of/index.js
@@ -0,0 +1,14 @@
+/* 
+for of arrays ,strings [algo]
+
+for(variable of objeto){
+código a ejecutar
+}
+
+*/
+
+let canasta = ["manzana","pera","naranja","uva"]
+
+for(fruta of canasta){
+    console.log(`La fruta es ${fruta}`)
+} 
