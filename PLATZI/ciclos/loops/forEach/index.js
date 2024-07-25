edcb37e1b288b931aaeb1f6cacb9c9a02ec09d78
Irving mtz <al2173074687@azc.uam.mx>
2024-07-25 02:57:30 -0600

    Estudio del ciclo forEach

---
+++ PLATZI/ciclos/loops/forEach/index.js
@@ -0,0 +1,12 @@
+/*
+
+array.forEach(() => {
+    código a ejecutar
+    })
+
+*/
+let frutas = ["Manzana","Platano","Uva","Sandia"]
+
+frutas.forEach((fruta, indice)=>{
+    console.log(`La fruta #${indice} es ${fruta}`)
+})
