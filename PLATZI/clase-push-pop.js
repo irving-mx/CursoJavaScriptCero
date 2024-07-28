5fc3ac2aa7119ffc0117d60a5f0af2ef46256ac5
Irving mtz <al2173074687@azc.uam.mx>
2024-07-28 04:49:27 -0600

    uso del metodo push y pop en arrays

---
+++ PLATZI/clase-push-pop.js
@@ -0,0 +1,15 @@
+// Methods that modifi the original array(Mutability)
+
+// push()
+
+const countries = ['USA','Canada','UK']
+const newCountries = countries.push('Germany','Australia')
+console.log(countries)
+console.log(newCountries)
+
+
+// pop()
+
+const removedCountry = countries.pop()
+console.log(countries)
+console.log(removedCountry)
