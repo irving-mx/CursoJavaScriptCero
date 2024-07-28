c37714450af088a1c76b3797bb2da34721bea1f4
Irving mtz <al2173074687@azc.uam.mx>
2024-07-28 04:31:38 -0600

    metodo push, concat, isArray y sumatoria

--- PLATZI/clase-introduccion-a-arrays.js
+++ PLATZI/clase-introduccion-a-arrays.js
@@ -40,3 +40,26 @@ console.log(firstFruit)
 // length property
 const numberOfFruits = fruits.length
 console.log(numberOfFruits)
+
+// Mutability
+fruits.push('Watermelon')
+console.log(fruits)
+
+// Inmutability
+const newFruits = fruits.concat(['grape','kiwi'])
+console.log(fruits)
+console.log(newFruits)
+
+// Checking arrays with Array.isArray()
+
+const isArray = Array.isArray(fruits)
+console.log(isArray)
+
+// Practical exercise: sum all elements of an array
+const numberArray = [1,2,3,4,5]
+let sum = 0
+
+for(let i =0; i < numberArray.length; i++){
+    sum+= numberArray[i]
+}
+console.log(`La sumatoria es: ${sum}`)
