08b48d3b9722f07c22b7b0903292c3356cec38a0
Irving mtz <al2173074687@azc.uam.mx>
2024-08-12 03:52:46 -0600

    Prototipos en la practica

--- PLATZI/objetos/prototypeYinheritance.js
+++ PLATZI/objetos/prototypeYinheritance.js
@@ -23,6 +23,31 @@ class Perro extends Animal{
 
 const perro1 = new Perro("Bobby","Perro","Pug");
 console.log(perro1)
+console.log("_________________")
 
 perro1.correr();
+console.log("_________________")
+
 perro1.emitirSonido();
+
+perro1.nuevoMetodo = function(){
+    console.log("Este es un método")
+}
+console.log("_________________")
+
+perro1.nuevoMetodo()
+console.log("_________________")
+
+console.log(perro1)
+
+Perro.prototype.segundoMetodo = function(){
+    console.log("Es otro nuevo metodo")
+}
+
+console.log(Perro)
+
+let currentPrototype = Object.getPrototypeOf(bast);
+for (;currentPrototype !== null; currentPrototype = Object.getPrototypeOf(currentPrototype)) {
+    console.log(currentPrototype);
+}
+
